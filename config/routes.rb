5ec21703f633d0a714a6c5f63300f19d490602b0
Majid Fakhoury <majidfakhoury@gmail.com>
2020-08-11 17:18:36 +0300

    all functions up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index', as: :task
+  get 'tasks', to: 'tasks#index'
 
-  get 'tasks/new', to: 'tasks#new'
-
-  get 'tasks/:id', to: 'tasks#find', as: :find
+  get 'tasks/new', to: 'tasks#new', as: :new
 
   post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id', to: 'tasks#find', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
